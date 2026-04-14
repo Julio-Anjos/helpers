fad82f9b03954fe2b23eaf3534c7d44288ba1447
Agaci <joseagaci35@gmail.com>
2026-04-14 19:26:29 -0300

    Delete cplat-beta1.0 directory

--- cplat-beta1.0/cplat/backend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove todas as variáveis de sessão
-session_destroy(); // Encerra a sessão
-header("Location: ../frontend/pages/bemvindos.php"); // Redireciona para a página de login ou home
-exit();
-?>
